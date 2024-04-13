6582bec845ed979477ce56a0afd11db6d6d6a8bb
Tom Fransen <t.w.j.fransen@student.utwente.nl>
2024-04-13 19:25:45 +0200

    Add debug image

--- src/relbot_control/include/relbot_control/velocity_controller.hpp
+++ src/relbot_control/include/relbot_control/velocity_controller.hpp
@@ -14,6 +14,7 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "geometry_msgs/msg/twist_stamped.hpp"
+#include "sensor_msgs/msg/image.hpp"
 //#include "../../../install/relbot_vision/include/relbot_vision/relbot_vision/msg/ball_detection.hpp"
 #include "relbot_vision/msg/ball_detection.hpp"
 
@@ -27,14 +28,20 @@ public:
 private:
     /// Callback functions.
     void detection_callback(relbot_vision::msg::BallDetection::ConstSharedPtr detection);
+    void image_callback(sensor_msgs::msg::Image::ConstSharedPtr image);
 
     /// Private variables.
+    bool debug;
+    float target_size = 200; // pixels
+    float target_position = 320; // pixels
 
     /// Subscriber variables.
     rclcpp::Subscription<relbot_vision::msg::BallDetection>::SharedPtr detection_sub;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub;
 
     /// Publisher variables.
     rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr velocity_pub;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr debugImage_pub;
 };
 
 #endif /* VELOCITY_CONTROLLER_HPP */
--- src/relbot_control/src/velocity_controller.cpp
+++ src/relbot_control/src/velocity_controller.cpp
@@ -10,29 +10,41 @@
 // #include "relbot_control/velocity_controller.hpp"
 #include <stdexcept>
 #include <algorithm>
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 using std::placeholders::_1;
 
 VelocityController::VelocityController() : Node("velocity_controller") {
-    // Set subscription image topic
+    // Set subscription detection topic
     this->declare_parameter<std::string>("detection_topic", "/ball_detector/detection");
     std::string detection_topic_name;
     this->get_parameter("detection_topic", detection_topic_name);
 
+    // Set subscription image topic
+    this->declare_parameter<std::string>("image_topic", "/ball_detector/debug_image");
+    std::string image_topic_name;
+    this->get_parameter("image_topic", image_topic_name);
+
+    // Set subscription image topic
+    this->declare_parameter<bool>("debug", false);
+    this->get_parameter("debug", debug);
+
+
     // Subscribe to topics
     detection_sub = this->create_subscription<relbot_vision::msg::BallDetection>(detection_topic_name, 10, std::bind(&VelocityController::detection_callback, this, _1));
+    if (debug) image_sub = this->create_subscription<sensor_msgs::msg::Image>(image_topic_name, 10, std::bind(&VelocityController::image_callback, this, _1));
 
     // Create topics to publish to
     velocity_pub = this->create_publisher<geometry_msgs::msg::TwistStamped>("velocity_controller/velocity", 10);
+    if (debug) debugImage_pub = this->create_publisher<sensor_msgs::msg::Image>("velocity_controller/debug_image", 10);
 }
 
 void VelocityController::detection_callback(relbot_vision::msg::BallDetection::ConstSharedPtr detection) {
-    float target_size = 200; // pixels
-    float target_position = 320; // pixels
     float size_error = target_size - std::max(detection->bounding_box.width, detection->bounding_box.height);
     float position_error = target_position - detection->bounding_box.centre_x;
 
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Size error: %f\tPosition error:%f", size_error, position_error);
+    RCLCPP_INFO(rclcpp::get_logger("velocity_controller"), "Size error: %f\tPosition error:%f", size_error, position_error);
 
     float linear_gain = 1; // meters per pixels per seconds
     float angular_gain = 1; // radians per pixels per seconds
@@ -44,6 +56,27 @@ void VelocityController::detection_callback(relbot_vision::msg::BallDetection::C
     velocity_pub->publish(velocity_msg);
 }
 
+void VelocityController::image_callback(sensor_msgs::msg::Image::ConstSharedPtr image) {
+    // Convert ROS message to openCV image
+    cv_bridge::CvImagePtr cv_ptr;
+    try {
+        cv_ptr = cv_bridge::toCvCopy(image, image->encoding);
+    } catch (cv_bridge::Exception& e) {
+        RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    // Calculate bounding box coordinates
+    cv::Point left_up(target_position - target_size/2, image->height/2 - target_size/2);
+    cv::Point right_down(target_position + target_size/2, image->height/2 + target_size/2);   
+
+    // Draw target bounding box
+    cv::rectangle(cv_ptr->image, left_up, right_down, cv::Scalar(0, 0, 255), 3);
+
+    cv::cvtColor(cv_ptr->image, cv_ptr->image, cv::COLOR_BGR2RGB);
+    sensor_msgs::msg::Image::SharedPtr msg = cv_ptr->toImageMsg();
+    debugImage_pub->publish(*msg);
+}
 
 // The main function starts the node and "spins" it, i.e. handles all ROS2-related events such as receiving messages on topics
 // You rarely need to add anything else to this function for ROS2 nodes
