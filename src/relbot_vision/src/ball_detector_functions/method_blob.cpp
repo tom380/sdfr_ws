70a23f0ab5c4b9100026f52a6311cb034145dabe
Tom Fransen <t.w.j.fransen@student.utwente.nl>
2024-04-26 12:33:34 +0200

    Split functionalities in seperate files

---
+++ src/relbot_vision/src/ball_detector_functions/method_blob.cpp
@@ -0,0 +1,57 @@
+#include "relbot_vision/ball_detector.hpp"
+#include <cv_bridge/cv_bridge.h>
+#include <string>
+
+relbot_vision::msg::BallDetection BallDetector::detect_blob(const sensor_msgs::msg::Image::ConstSharedPtr& img) {
+    // Convert ROS message to openCV image
+    cv_bridge::CvImagePtr cv_ptr;
+    try {
+        cv_ptr = cv_bridge::toCvCopy(img, img->encoding);
+    } catch (cv_bridge::Exception& e) {
+        std::string error_msg = e.what();
+        throw new cv_bridge::Exception("cv_bridge exception: " + error_msg);
+    }
+
+    if (cv_ptr->encoding == "rgb8") {
+        cv::cvtColor(cv_ptr->image, cv_ptr->image, cv::COLOR_RGB2BGR);
+    }
+    
+    // Convert image to the HSV color space for easy colour selection
+    cv::Mat hsv_image;
+    cv::cvtColor(cv_ptr->image, hsv_image, cv::COLOR_BGR2HSV);
+
+    // Create mask based on colour
+    cv::Mat mask;
+    cv::inRange(hsv_image, cv::Scalar(hue - 10, 128, 50), cv::Scalar(hue + 10, 255, 255), mask);
+
+    // Clean-up mask
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE,cv::Size(5,5));
+    cv::erode(mask, mask, kernel, cv::Point(-1, -1), 3);
+    cv::dilate(mask, mask, kernel, cv::Point(-1, -1), 3);
+
+    // Detect blobs
+    std::vector<cv::KeyPoint> keypoints;
+    detector->detect(mask, keypoints);
+
+    // Initialize ball_detection message
+    relbot_vision::msg::BallDetection ball_detection_msg;
+    ball_detection_msg.found = false;
+
+    // Set bounding_box and found if circles have been found
+    if (!keypoints.empty()) {
+        // Assuming first keypoint is the ball
+        const cv::KeyPoint& keypoint = keypoints[0];
+
+        // Create bounding_box message
+        relbot_vision::msg::BoundingBox bouding_box_msg;
+        bouding_box_msg.centre_x = keypoint.pt.x;
+        bouding_box_msg.centre_y = keypoint.pt.y;
+        bouding_box_msg.width = bouding_box_msg.height = keypoint.size;
+
+        // Set ball_detection message
+        ball_detection_msg.found = true;
+        ball_detection_msg.bounding_box = bouding_box_msg;
+    }
+
+    return ball_detection_msg;
+}
--- src/relbot_vision/src/ball_detector_functions/utils.cpp
+++ src/relbot_vision/src/ball_detector_functions/utils.cpp
@@ -1,32 +1,5 @@
 #include "relbot_vision/ball_detector.hpp"
-
-cv::Mat BallDetector::colourMask(cv::Mat original_image, cv::Mat mask) {
-    // Convert the original image to grayscale
-    cv::Mat gray_image;
-    cv::cvtColor(original_image, gray_image, cv::COLOR_BGR2GRAY);
-    // Convert back to BGR for bitwise operations
-    cv::cvtColor(gray_image, gray_image, cv::COLOR_GRAY2BGR);
-
-    // Initialize the result image
-    cv::Mat result = cv::Mat::zeros(original_image.size(), original_image.type());
-
-    // Apply the mask to the original image to get the colored regions
-    cv::Mat colored_parts;
-    cv::bitwise_and(original_image, original_image, colored_parts, mask);
-
-    // Invert the mask to combine with the grayscale image
-    cv::Mat inverted_mask;
-    cv::bitwise_not(mask, inverted_mask);
-
-    // Apply the inverted mask to the grayscale image to get the grayscale regions
-    cv::Mat gray_parts;
-    cv::bitwise_and(gray_image, gray_image, gray_parts, inverted_mask);
-
-    // Combine the two parts
-    cv::add(colored_parts, gray_parts, result);
-
-    return result;
-}
+#include "image_functions_sdfr/image_functions.hpp"
 
 BallDetector::Method BallDetector::stringToMethod(const std::string& mode) const {
     // Return the matching method
@@ -35,4 +8,50 @@ BallDetector::Method BallDetector::stringToMethod(const std::string& mode) const
     else if (mode == "NOCV") return Method::NOCV;
     // If no matching method has been found throw invalid_argument error, to indicate wrong method
     else throw std::invalid_argument("Unknown detection mode");
+}
+
+void BallDetector::setBlobDetector() {
+    // Blob detector parameters
+    cv::SimpleBlobDetector::Params params;
+    params.filterByArea = true;
+    params.minArea = 100;
+    params.maxArea = std::numeric_limits<float>::infinity();
+    params.filterByCircularity = true;
+    params.minCircularity = 0.6;
+    params.filterByColor = false;
+    params.filterByConvexity = false;
+    params.filterByInertia = false;
+
+    // Create a blob detector with the above parameters
+    detector = cv::SimpleBlobDetector::create(params);
+}
+
+sensor_msgs::msg::Image::SharedPtr BallDetector::generateDebug(const sensor_msgs::msg::Image::ConstSharedPtr& img, const relbot_vision::msg::BoundingBox& bounding_box) {
+    sensor_msgs::msg::Image::SharedPtr debug_img = std::make_shared<sensor_msgs::msg::Image>();
+    image_functions::copyImageProperties(debug_img, img);
+    debug_img->data = img->data;
+
+    const int minX = bounding_box.centre_x - bounding_box.width / 2.0;
+    const int maxX = bounding_box.centre_x + bounding_box.width / 2.0;
+    const int minY = bounding_box.centre_y - bounding_box.height / 2.0;
+    const int maxY = bounding_box.centre_y + bounding_box.height / 2.0;
+
+    for (int x = minX; x < maxX; x++) {
+        for (int i = -2; i <= 2; i++) {
+            if (minY + i >= 0 && minY + i < (int)debug_img->width)
+                image_functions::setPixelColor(debug_img, x, minY + i, 0, 0, 255);
+            if (maxY + i >= 0 && maxY + i < (int)debug_img->width)                            
+                image_functions::setPixelColor(debug_img, x, maxY + i, 0, 0, 255);
+        }
+    }
+    for (int y = minY; y < maxY; y++) {
+        for (int i = -2; i <= 2; i++) {
+            if (minX + i >= 0 && minX + i < (int)debug_img->width)
+                image_functions::setPixelColor(debug_img, minX + i, y, 0, 0, 255);
+            if (maxX + i >= 0 && maxX + i < (int)debug_img->width)
+                image_functions::setPixelColor(debug_img, maxX + i, y, 0, 0, 255);
+        }
+    }
+
+    return debug_img;
 }
